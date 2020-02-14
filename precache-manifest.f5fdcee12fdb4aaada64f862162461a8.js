f9eff4800010006015839c9a173fcd001fb220ae
Chris Chan <chrischan226@gmail.com>
2020-02-14 13:40:10 -0800

    Updates

---
+++ precache-manifest.f5fdcee12fdb4aaada64f862162461a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2a6ef9d8ebdb7a7beab65e7c9f5bb7",
+    "url": "/chrischan226/inventory_parser/index.html"
+  },
+  {
+    "revision": "d9a3d872ce7304878857",
+    "url": "/chrischan226/inventory_parser/static/css/main.6f5f6bbe.chunk.css"
+  },
+  {
+    "revision": "cfa254f557df55c427cf",
+    "url": "/chrischan226/inventory_parser/static/js/2.f9b8d9c3.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/chrischan226/inventory_parser/static/js/2.f9b8d9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a3d872ce7304878857",
+    "url": "/chrischan226/inventory_parser/static/js/main.fc1d75ba.chunk.js"
+  },
+  {
+    "revision": "0111ec3b725154b00442",
+    "url": "/chrischan226/inventory_parser/static/js/runtime-main.f200f244.js"
+  }
+]);
